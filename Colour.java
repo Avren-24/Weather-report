11f83ebd84bb4f9accb1ccf26bebe18b924311b5
Avren-24 <202483890024@nuist.edu.cn>
2024-12-19 18:59:05 +0800

    Update Colour.java

--- Colour.java
+++ Colour.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Colour {
-    private static final String[] Colour = {
+    private static final String[] COLOURS = {
             "\u001B[91m", // Bright Red
             "\u001B[92m", // Bright Green
             "\u001B[93m", // Bright Yellow
@@ -13,14 +13,23 @@ public class Colour {
 
     private static final Random random = new Random();
 
-    public static void printRandomColor(String Word) {
-        String randomColor = Colour[random.nextInt(Colour.length)];
-        System.out.print(randomColor + Word + "\u001B[0m"); //Reset the color
+    public static void printRandomColor(String word) {
+        String randomColor = COLOURS[random.nextInt(COLOURS.length)];
+        System.out.print(randomColor + word + "\u001B[0m");
     }
 
-    public static void printlnRandomColor(String Wordln) {
-        String randomColor = Colour[random.nextInt(Colour.length)];
-        System.out.println(randomColor + Wordln + "\u001B[0m"); //Reset the color
+    public static void printlnRandomColor(String wordln) {
+        String randomColor = COLOURS[random.nextInt(COLOURS.length)];
+        System.out.println(randomColor + wordln + "\u001B[0m");
+    }
+
+    public static void printRandomColorForPattern(String pattern) {
+        StringBuilder result = new StringBuilder();
+        for (char signal : pattern.toCharArray()) {
+                String randomColor = COLOURS[random.nextInt(COLOURS.length)];
+                result.append(randomColor).append(signal).append("\u001B[0m");
+        }
+        System.out.println(result.toString());
     }
 }
 // End of Colour Class
